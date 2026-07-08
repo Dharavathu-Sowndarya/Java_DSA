6d09a2676a2eaa30ea1a659b39c12bd19284883b
Dharavathu-Sowndarya <154575766+Dharavathu-Sowndarya@users.noreply.github.com>
2026-07-08 15:23:24 +0530

    Add RotateArray class for array rotation functionality
    Implement RotateArray class to rotate an array based on user input.

---
+++ Arrays/RotateArray.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class RotateArray {
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter number of rotations: ");
+        int k = sc.nextInt();
+
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+
+        System.out.println("Array after rotation:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
